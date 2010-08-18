ffa2384e1ced8a53da97936b1544e14b85e39b8e
Tom Elam <tomelam@gmail.com>
2010-08-18 17:38:05 +0530

    Add back all modules in module.js

--- code/tests/module.js
+++ code/tests/module.js
@@ -4,16 +4,14 @@
 dojo.provide("code.tests.module");
 
 try{
-    //dojo.require("code.tests.new");
-    //dojo.require("code.tests.mutable");
-    //dojo.require("code.tests.json");
+    dojo.require("code.tests.new");
+    dojo.require("code.tests.mutable");
+    dojo.require("code.tests.json");
     dojo.require("code.tests.primitives");
-/*
     dojo.require("code.tests.equality");
     dojo.require("code.tests.arrays");
     dojo.require("code.tests.oo");
     dojo.require("code.tests.michaux-oop");
-*/
 }catch(e){
     doh.debug(e);
 }
